3ad21dff3d111d1abe4ddb497a0754018e0abf97
Rejoy <rejoy.kulangara@ibm.com>
2026-05-04 11:13:41 +0530

    fixed compilation error

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/cdi/ErrorCode.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/cdi/ErrorCode.java
@@ -32,7 +32,7 @@ public enum ErrorCode implements IJavaErrorCode {
     InvalidInjectAnnotatedMethodParamAnnotation,
     InvalidInjectAnnotationOnMultipleMethodParams,
     InvalidObservesObservesAsyncMethodParams,
-    InvalidDependentScopeWithConditionalObserver;
+    InvalidDependentScopeWithConditionalObserver,
     InvalidStatelessSessionBeanScope;
 
     /**
